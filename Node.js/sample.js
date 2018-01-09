1c8a202467ea49b7e9468ec47cf4aaf2f0e9c1e9
Noob Tsai <NoobTW@users.noreply.github.com>
2018-01-09 10:35:55 +0800

    Add Node.js Sample (#3)
    * Update README.md
    
    * Update JavaScript spelling
    
    * Add Node.js example
    
    * Update README.md

---
+++ Node.js/sample.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+const jsSHA = require('jssha');
+
+const getAuthorizationHeader = function() {
+	var AppID = 'FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF';
+	var AppKey = 'FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF';
+
+	var GMTString = new Date().toGMTString();
+	var ShaObj = new jsSHA('SHA-1', 'TEXT');
+	ShaObj.setHMACKey(AppKey, 'TEXT');
+	ShaObj.update('x-date: ' + GMTString);
+	var HMAC = ShaObj.getHMAC('B64');
+	var Authorization = 'hmac username=\"' + AppID + '\", algorithm=\"hmac-sha1\", headers=\"x-date\", signature=\"' + HMAC + '\"';
+
+	return { 'Authorization': Authorization, 'X-Date': GMTString};
+}
+
+axios.get('http://ptx.transportdata.tw/MOTC/v2/Rail/TRA/Station?$top=10&$format=JSON', { // 欲呼叫之API網址(此範例為台鐵車站資料)
+	headers: getAuthorizationHeader(),
+})
+	.then(function(response){
+		console.log(response.data);
+	});
