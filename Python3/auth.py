1fc083605ebde0f95369c793831d3cbd24c4831d
Rayer <rayershih@gmail.com>
2018-07-19 04:05:10 -0500

    Sample code for Python3 -- adopted from Wesely provided sample (#6)

---
+++ Python3/auth.py
@@ -0,0 +1,38 @@
+from hashlib import sha1
+import hmac
+from wsgiref.handlers import format_date_time
+from datetime import datetime
+from time import mktime
+import base64
+from requests import request
+from pprint import pprint
+
+app_id = 'YOUR APP ID(L1)'
+app_key = 'YOUR APP KEY(L1)'
+
+class Auth():
+
+    def __init__(self, app_id, app_key):
+        self.app_id = app_id
+        self.app_key = app_key
+
+    def get_auth_header(self):
+        xdate = format_date_time(mktime(datetime.now().timetuple()))
+        hashed = hmac.new(self.app_key.encode('utf8'), ('x-date: ' + xdate).encode('utf8'), sha1)
+        signature = base64.b64encode(hashed.digest()).decode()
+
+        authorization = 'hmac username="' + self.app_id + '", ' + \
+                        'algorithm="hmac-sha1", ' + \
+                        'headers="x-date", ' + \
+                        'signature="' + signature + '"'
+        return {
+            'Authorization': authorization,
+            'x-date': format_date_time(mktime(datetime.now().timetuple())),
+            'Accept - Encoding': 'gzip'
+        }
+
+
+if __name__ == '__main__':
+    a = Auth(app_id, app_key)
+    response = request('get', 'http://ptx.transportdata.tw/MOTC/v2/Bus/Stop/City/Taipei?$top=30&$format=JSON', headers= a.get_auth_header())
+    pprint(response.content)
