3de7e3c0d38d6877fb50c9615fdd645681528504
PTX <ptx.motc@gmail.com>
2019-05-11 13:43:27 +0800

    Update Sample.js

--- JavaScript/Sample.js
+++ JavaScript/Sample.js
@@ -1,7 +1,7 @@
 $(function () {
     $.ajax({
         type: 'GET',
-        url: 'http://ptx.transportdata.tw/MOTC/v2/Rail/TRA/Station?$top=10&$format=JSON', //欲呼叫之API網址(此範例為台鐵車站資料)
+        url: 'https://ptx.transportdata.tw/MOTC/v2/Rail/TRA/Station?$top=10&$format=JSON', //欲呼叫之API網址(此範例為台鐵車站資料)
         dataType: 'json',
         headers: GetAuthorizationHeader(),
         success: function (Data) {
