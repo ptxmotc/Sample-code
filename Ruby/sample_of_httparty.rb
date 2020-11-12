4e9f7c756aa5931b35aaa2bf65701488fa0f7957
Rumble Huang <r04921058@ntu.edu.tw>
2020-11-12 17:47:44 +0800

    Enhance: share headers between APIs

--- Ruby/sample_of_httparty.rb
+++ Ruby/sample_of_httparty.rb
@@ -6,25 +6,33 @@ require 'httparty' # https://github.com/jnunemaker/httparty
 class PtxApiService
   include HTTParty
   base_uri 'https://ptx.transportdata.tw/MOTC/v2'
-  headers 'Content-Type' => 'application/json', 'Accept' => 'application/json'
   format :json
 
-  def tra_station
-    self.class.get('/Rail/TRA/Station?$top=10&$format=JSON', headers: authorization_headers).parsed_response
+  headers(
+    'Content-Type'  => 'application/json',
+    'Accept'        => 'application/json',
+    'X-Date'        => ->{ @current_timestamp = Time.now.utc.strftime('%a, %d %b %Y %T GMT') },
+    'Authorization' => ->{ authorization_header },
+  )
+
+  class << self
+    def authorization_header
+      hmac = Base64.strict_encode64(OpenSSL::HMAC.digest('sha1', APP_KEY, "x-date: #{@current_timestamp}"))
+      return %(hmac username="#{APP_ID}", algorithm="hmac-sha1", headers="x-date", signature="#{hmac}")
+    end
   end
 
-  private
+  def daily_time_table(date)
+    self.class.get("/Rail/THSR/DailyTimetable/TrainDate/#{date.strftime('%F')}?$top=1&$format=JSON").parsed_response
+  end
 
-  def authorization_headers
-    timestamp = Time.now.utc.strftime('%a, %d %b %Y %T GMT')
-    hmac = Base64.strict_encode64(OpenSSL::HMAC.digest('sha1', APP_KEY, "x-date: #{timestamp}"))
-
-    return {
-      'X-Date'        => timestamp,
-      'Authorization' => %(hmac username="#{APP_ID}", algorithm="hmac-sha1", headers="x-date", signature="#{hmac}"),
-    }
+  def tra_station
+    self.class.get('/Rail/TRA/Station?$top=1&$format=JSON').parsed_response
   end
 end
 
-response = PtxApiService.new.tra_station
-# =>  [{"StationUID"=>"TRA-1001", ... }]
+response1 = PtxApiService.new.daily_time_table(Date.new(2020, 11, 12))
+# => [{"TrainDate"=>"2020-11-12", ... }]
+
+response2 = PtxApiService.new.tra_station
+# => [{"StationUID"=>"TRA-1001", ... }]
